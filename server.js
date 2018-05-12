6db06d9071693831f777c72fcd7e4aa232a4b641
BBITguru <jparker@bbit.guru>
2018-05-11 22:57:22 -0400

    deploying to heroku

---
+++ server.js
@@ -0,0 +1,7 @@
+// Set Port, hosting services will look for process.env.PORT
+app.set('port', (process.env.PORT || 3000));
+
+// start the server
+app.listen(app.get('port'), () => {
+  console.log(`Server is running on port ${app.get('port')}`);
+});
